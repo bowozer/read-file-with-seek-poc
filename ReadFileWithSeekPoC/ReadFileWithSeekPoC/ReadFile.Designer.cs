2ea52925db17ce2ac6c79f87296070f7bd084e27
Ardhio Wibowo <ardhio.sutjipto@mitrais.com>
2021-03-08 12:55:19 +0800

    Initial commit. ReadFile with Seek(...)
    It reads file from FileToRead.txt.
    
    Just run the app and then add some lines to FileToRead.txt. The texts to
    add can be used from ExampleFileToRead.txt

---
+++ ReadFileWithSeekPoC/ReadFileWithSeekPoC/ReadFile.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ReadFileWithSeekPoC
+{
+    partial class ReadFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Read = new System.Windows.Forms.Button();
+            this.FileContent = new System.Windows.Forms.TextBox();
+            this.UpdatedTimestampLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Read
+            // 
+            this.Read.Location = new System.Drawing.Point(373, 10);
+            this.Read.Name = "Read";
+            this.Read.Size = new System.Drawing.Size(75, 23);
+            this.Read.TabIndex = 0;
+            this.Read.Text = "Read";
+            this.Read.UseVisualStyleBackColor = true;
+            this.Read.Click += new System.EventHandler(this.Read_Click);
+            // 
+            // FileContent
+            // 
+            this.FileContent.Location = new System.Drawing.Point(12, 12);
+            this.FileContent.Multiline = true;
+            this.FileContent.Name = "FileContent";
+            this.FileContent.Size = new System.Drawing.Size(355, 321);
+            this.FileContent.TabIndex = 1;
+            // 
+            // UpdatedTimestampLbl
+            // 
+            this.UpdatedTimestampLbl.AutoSize = true;
+            this.UpdatedTimestampLbl.Location = new System.Drawing.Point(373, 36);
+            this.UpdatedTimestampLbl.Name = "UpdatedTimestampLbl";
+            this.UpdatedTimestampLbl.Size = new System.Drawing.Size(35, 13);
+            this.UpdatedTimestampLbl.TabIndex = 2;
+            this.UpdatedTimestampLbl.Text = "label1";
+            // 
+            // ReadFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UpdatedTimestampLbl);
+            this.Controls.Add(this.FileContent);
+            this.Controls.Add(this.Read);
+            this.Name = "ReadFile";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ReadFile_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Read;
+        private System.Windows.Forms.TextBox FileContent;
+        private System.Windows.Forms.Label UpdatedTimestampLbl;
+    }
+}
+
---
+++ ReadFileWithSeekPoC/ReadFileWithSeekPoC/ReadFile.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadFileWithSeekPoC
+{
+    /// <summary>
+    /// Read file with seek, so it doesn't read all of the files.
+    /// 
+    /// github: bowozer
+    /// </summary>
+    public partial class ReadFile : Form
+    {
+        const string FILE_PATH = @"..\..\..\FileToRead.txt";
+        // in seconds
+        private const int READ_FILE_TIMER = 10;
+
+        private long _lastPosition = 0;
+        private DateTime _lastReadTime = DateTime.MinValue;
+        private delegate void RefreshTimestampTextDelegate();
+        private delegate void AppendTextDelegate(string text);
+
+        public ReadFile()
+        {
+            InitializeComponent();
+        }
+
+        private void Read_Click(object sender, EventArgs e)
+        {
+            ReadTheFile();
+        }
+
+        private void ReadFile_Load(object sender, EventArgs e)
+        {
+            // Task that run forever
+            var task = Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    ReadTheFile();
+                    Thread.Sleep(READ_FILE_TIMER * 1000);
+                }
+            });
+        }
+
+        private void ReadTheFile()
+        {
+            using (FileStream fileStream = new FileStream(FILE_PATH, FileMode.Open))
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                fileStream.Seek(_lastPosition, SeekOrigin.Begin);
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    AppendTextThreadSafe(line);
+                }
+
+                _lastPosition = fileStream.Position;
+            }
+
+            _lastReadTime = DateTime.Now;
+            RefreshTimestampThreadSafe();
+        }
+
+        private void RefreshTimestampThreadSafe()
+        {
+            if (UpdatedTimestampLbl.InvokeRequired)
+            {
+                UpdatedTimestampLbl.Invoke(new RefreshTimestampTextDelegate(RefreshTimestampText));
+            }
+            else
+            {
+                RefreshTimestampText();
+            }
+        }
+
+        private void AppendTextThreadSafe(string line)
+        {
+            if (FileContent.InvokeRequired)
+            {
+                FileContent.Invoke(new AppendTextDelegate(FileContent.AppendText), line);
+                FileContent.Invoke(new AppendTextDelegate(FileContent.AppendText), Environment.NewLine);
+            }
+            else
+            {
+                FileContent.AppendText(line);
+                FileContent.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void RefreshTimestampText()
+        {
+            UpdatedTimestampLbl.Text = $"Updated at {_lastReadTime:u}";
+        }
+    }
+}
